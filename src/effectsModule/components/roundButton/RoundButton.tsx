9fa205c8f30b259469d41007c3aa3bf4d11aa4cb
Paul Jacks <Rebstorm@users.noreply.github.com>
2021-04-10 16:29:54 +0200

    I figured out react springs, lol

--- src/effectsModule/components/roundButton/RoundButton.tsx
+++ src/effectsModule/components/roundButton/RoundButton.tsx
@@ -1,5 +1,6 @@
-import { Component } from "react";
+import { FunctionComponent } from "react";
 import "./RoundButton.scss";
+import { useSpring, animated } from "react-spring";
 
 interface ButtonProps {
   color: string;
@@ -8,23 +9,35 @@ interface ButtonProps {
   clickChildEvent: (e: React.MouseEvent<HTMLDivElement>) => void;
 }
 
-interface ButtonState {}
+const calc = (x: number, y: number) => [
+  -(y - window.innerHeight / 2) / 20,
+  (x - window.innerWidth / 2) / 20,
+  1.1,
+];
+const trans = (x: number, y: number, s: number) =>
+  `perspective(600px) rotateX(${x}deg) rotateY(${y}deg) scale(${s})`;
 
-export class RoundButton extends Component<ButtonProps, ButtonState> {
-  render() {
-    return (
-      <div
-        onClick={(e) => this.props.clickChildEvent(e)}
-        className={"round"}
-        style={{
-          backgroundColor: this.props.color,
-          width: this.props.sizeInPercent + "%",
-          height: this.props.sizeInPercent + "%",
-          zIndex: this.props.zIndex,
-        }}
-      >
-        RoundButton
-      </div>
-    );
-  }
-}
+export const RoundButton: FunctionComponent<ButtonProps> = (b) => {
+  const [props, set] = useSpring(() => ({
+    xys: [0, 0, 1],
+    config: { mass: 5, tension: 350, friction: 40 },
+  }));
+
+  return (
+    <animated.div
+      onClick={(e) => b.clickChildEvent(e)}
+      className={"round"}
+      onMouseMove={({ clientX: x, clientY: y }) => set({ xys: calc(x, y) })}
+      onMouseLeave={() => set({ xys: [0, 0, 1] })}
+      style={{
+        backgroundColor: b.color,
+        width: b.sizeInPercent + "%",
+        height: b.sizeInPercent + "%",
+        zIndex: b.zIndex,
+        transform: props.xys.to(trans),
+      }}
+    >
+      RoundButton
+    </animated.div>
+  );
+};
