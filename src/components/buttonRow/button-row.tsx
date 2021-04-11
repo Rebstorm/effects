4116900928092a3e44ad7eed3511766651634219
Paul Jacks <Rebstorm@users.noreply.github.com>
2021-04-11 23:02:32 +0200

    more code

--- src/components/buttonRow/button-row.tsx
+++ src/components/buttonRow/button-row.tsx
@@ -1,5 +1,4 @@
 import React, { FunctionComponent } from "react";
-import { Link, useHistory } from "react-router-dom";
 import "./button-row.scss";
 import { MenuButton, MenuButtonProps } from "./menuButton/menu-button";
 
@@ -12,40 +11,30 @@ export const ButtonRow: FunctionComponent<TitleProps> = (props) => {
     {
       id: "about",
       zIndex: 2,
-      color: "#905AFA",
+      color: "#2EC9E6",
       sizeInPercent: 30,
-      clickChildEvent: (e, id) => {},
     },
     {
       id: "tech",
       zIndex: 1,
-      color: "#5950DE",
+      color: "#4D806B",
       sizeInPercent: 60,
-      clickChildEvent: (e, id) => {},
     },
     {
       id: "copyright",
       zIndex: 0,
-      color: "#6481F5",
+      color: "#FA5950",
       sizeInPercent: 90,
-      clickChildEvent: (e, id) => {},
     }
   );
-
-  const history = useHistory();
-
-  function handleClick(e: React.MouseEvent<HTMLDivElement>, id: string) {
-    history.push(`/${id}`);
-  }
   return (
     <>
-      {buttonArray.map((item, key, prop) => (
+      {buttonArray.map((item) => (
         <MenuButton
           id={item.id}
           color={item.color}
           sizeInPercent={item.sizeInPercent}
           zIndex={item.zIndex}
-          clickChildEvent={(e, id) => handleClick(e, id)}
         />
       ))}
     </>
--- src/components/buttonRow/menuButton/menu-button.tsx
+++ src/components/buttonRow/menuButton/menu-button.tsx
@@ -1,16 +1,17 @@
-import React, { FunctionComponent, useEffect } from "react";
+import React, { FunctionComponent, useEffect, useState } from "react";
 import "./menu-button.scss";
-import { animated } from "react-spring";
+import { animated, useSpring } from "react-spring";
+import { useHistory } from "react-router-dom";
 
 export interface MenuButtonProps {
   id: string;
   color: string;
   sizeInPercent: number;
   zIndex?: number;
-  clickChildEvent: (e: React.MouseEvent<HTMLDivElement>, id: string) => void;
 }
 
 export const MenuButton: FunctionComponent<MenuButtonProps> = (prop) => {
+  const history = useHistory();
   useEffect(() => {
     // mount
     return () => {
@@ -18,18 +19,32 @@ export const MenuButton: FunctionComponent<MenuButtonProps> = (prop) => {
     };
   });
 
+  const [clicked, set] = useState(false);
+  const animProps = useSpring({
+    width: clicked ? "100%" : `${prop.sizeInPercent}%`,
+    height: clicked ? "100vh" : `${prop.sizeInPercent}%`,
+    zIndex: clicked ? 200 : prop.zIndex,
+    immediate: (key) => key === "zIndex",
+  });
   return (
     <animated.div
       onClick={(e) => {
-        // callback for listener
-        prop.clickChildEvent(e, prop.id);
+        // Router Change
+        /*
+      setTimeout(() => {
+        history.push(`/${prop.id}`);
+      }, 3000);
+       */
       }}
+      onMouseDown={() => {
+        set(true);
+      }}
+      onMouseUp={() => window.setTimeout(() => set(false), 300)}
       className={"round"}
       style={{
         backgroundColor: prop.color,
-        width: prop.sizeInPercent + "%",
-        height: prop.sizeInPercent + "%",
-        zIndex: prop.zIndex,
+        backgroundImage: `url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='8' height='8' viewBox='0 0 8 8'%3E%3Cg fill='%239C92AC' fill-opacity='0.4'%3E%3Cpath fill-rule='evenodd' d='M0 0h4v4H0V0zm4 4h4v4H4V4z'/%3E%3C/g%3E%3C/svg%3E"`,
+        ...animProps,
       }}
     >
       {prop.id}
