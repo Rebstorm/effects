7ae8aa87adbc6bc153f15758d2333b07102309af
Paul Jacks <Rebstorm@users.noreply.github.com>
2021-04-13 14:42:21 +0200

    optional wavy animation

--- src/components/buttonRow/menuButton/menu-button.tsx
+++ src/components/buttonRow/menuButton/menu-button.tsx
@@ -50,7 +50,7 @@ export const MenuButton: FunctionComponent<MenuButtonProps> = (prop) => {
         }}
       >
         <div
-          className={"wave"}
+          className={"wavy"}
           style={{
             width: "100%",
             height: "7rem",
@@ -67,7 +67,7 @@ export const MenuButton: FunctionComponent<MenuButtonProps> = (prop) => {
               fill={prop.color}
               fillOpacity="1"
               d="M0,192L48,197.3C96,203,192,213,288,229.3C384,245,480,267,576,250.7C672,235,768,181,864,181.3C960,181,1056,235,1152,234.7C1248,235,1344,181,1392,154.7L1440,128L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"
-            ></path>
+            />
           </svg>
         </div>
         <div
